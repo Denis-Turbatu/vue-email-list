cc99fced6f6a657d41130ee51138f95b4ef1fc40
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-29 14:35:07 +0200

    feat: created 10 random emails in array

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            res: [],
+        }
+    },
+    created(){
+        for (let i = 0; i < 9; i++) {
+            this.generateEmail();
         }
     },
     methods: {
-        generateEmail: function(res) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
-                const res = [];
-                res.push(result.data.response);
-                console.log(res);
-                return res;
-            })
+        generateEmail: function(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+                this.res.push(result.data.response);
+                console.log(this.res);
+            });
         },
     }
 }).mount('#app');
